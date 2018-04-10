bea818ed5a4c4bf3fb55ffd464beb0d4a96eae0b
mkunadavy <mkunadavy@gmail.com>
2018-04-10 18:24:36 +0200

    commited

--- app/src/main/java/come/example/regulatehct/entapp/MainActivity.java
+++ app/src/main/java/come/example/regulatehct/entapp/MainActivity.java
@@ -1,5 +1,6 @@
 package come.example.regulatehct.entapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,12 +82,15 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
+
+            Intent intent = new Intent(MainActivity.this,ScrollingFootballActivity.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_share) {
 
