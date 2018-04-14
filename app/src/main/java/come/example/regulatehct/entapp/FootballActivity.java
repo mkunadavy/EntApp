860b609ecc7fc37a7c987750419c23863bfb16d8
mkunadavy <mkunadavy@gmail.com>
2018-04-14 16:48:11 +0200

    commited 2

--- app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
+++ app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
@@ -1,7 +1,9 @@
 package come.example.regulatehct.entapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FootballActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FootballActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_football);
+    }
+
+    public void goBack(View view) {
+        Intent intent = new Intent(FootballActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
