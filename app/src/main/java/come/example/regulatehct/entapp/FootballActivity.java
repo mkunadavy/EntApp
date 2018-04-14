e171aec2ccd3878d2ab9f42cddac35f1ffa4cb18
mkunadavy <mkunadavy@gmail.com>
2018-04-14 17:03:26 +0200

    commited 3

--- app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
+++ app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class FootballActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,12 @@ public class FootballActivity extends AppCompatActivity {
     public void goBack(View view) {
         Intent intent = new Intent(FootballActivity.this,MainActivity.class);
         startActivity(intent);
+    }
+
+    public void setLog(View view) {
+        textView = (TextView) findViewById(R.id.table);
+
+        textView.setTextColor(000);
+        textView.setBackgroundColor(111);
     }
 }
