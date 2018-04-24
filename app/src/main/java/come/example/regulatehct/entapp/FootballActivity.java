194d2b71962c9733ae770ffe03b407b279f76038
mkunadavy <mkunadavy@gmail.com>
2018-04-24 14:51:26 +0200

    c5

--- app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
+++ app/src/main/java/come/example/regulatehct/entapp/FootballActivity.java
@@ -1,30 +1,42 @@
 package come.example.regulatehct.entapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class FootballActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    TextView textView;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class FootballActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_football);
+
     }
 
     public void goBack(View view) {
         Intent intent = new Intent(FootballActivity.this,MainActivity.class);
         startActivity(intent);
-    }
-
-    public void setLog(View view) {
-        textView = (TextView) findViewById(R.id.table);
-
-        textView.setTextColor(000);
-        textView.setBackgroundColor(111);
     }
 }
---
+++ app/src/main/java/come/example/regulatehct/entapp/Team.java
@@ -0,0 +1,9 @@
+package come.example.regulatehct.entapp;
+
+/**
+ * Created by regulatehct on 16/4/2018.
+ */
+
+public class Team {
+    private String name,venue;
+}
